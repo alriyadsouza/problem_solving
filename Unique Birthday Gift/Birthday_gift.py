725f88143559d0f39f86b0f2ea56639cadea3863
alriyadsouza <alriyatreeza929@gmail.com>
2024-08-03 21:37:57 +0530

    final birthday gift

--- Unique Birthday Gift/Birthday_gift.py
+++ Unique Birthday Gift/Birthday_gift.py
@@ -1,14 +1,19 @@
 class solution:
     @staticmethod
-    def output(n,a[k]):
-        for i in range(1,n+1):
-            for j in range(1,n+1):
-                if j%i == 0:
-                    a.append([i,j])
+    def output(n,a):
+        if k==1:
+            for i in range(1,n+1):
+                a.append([i])
+        else:
+            for i in range(1,n+1):
+                for j in range(1,n+1):
+                    if j%i == 0:
+                        a.append([i,j])
+        return len(a)
         
 n=int(input())
 k=int(input())
 a=[]
 Solution=solution()
-result=Solution.output(n,a[k])
+result=Solution.output(n,a)
 print(result)
